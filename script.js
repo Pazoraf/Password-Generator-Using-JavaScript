d1aea45aa39cb93b3343df2f9371daeb4b33ac28
Pazoraf <alexander.flaherty@hotmail.com>
2024-01-07 16:07:51 +0000

    Formatted code and added comments for better readability.

--- script.js
+++ script.js
@@ -87,14 +87,18 @@ var upperCasedCharacters = [
   'Y',
   'Z'
 ];
-
+// Empty array to store selected characters to be used
 var selectedCharacters = [];
 
+// Prompt to store desired password length as a variable
 var passwordLength = prompt("How many characters should your password consist of?");
+
+// While loop to check if password length is at least 8 characters and not greater than 128
 while (passwordLength<8 || passwordLength >128){
-  passwordLength = prompt("Your password needs to be between 8 and 128 characters")
+  passwordLength = prompt("Your password needs to be between 8 and 128 characters");
 }
 
+//Object to store characters to be used from user confirmation.
 var includes = {
   special: confirm("Do you want to include special characters?"),
   numeric: confirm("Do you want to include numeric characters?"),
@@ -102,18 +106,21 @@ var includes = {
   upperCase: confirm("Do you want to include uppercase characters?"),
 }
 
-
 // Function to prompt user for password options
 function getPasswordOptions(arr) {
   selectedCharacters.push(...arr);
 }
+
+// While loop to check if at least 1 character type is being used
 while (Object.values(includes).every((v) => v === false)){
-  alert("You need to include at least one character type!")
-  includes.special = confirm("Do you want to include special characters?")
-  includes.numeric = confirm("Do you want to include numeric characters?")
-  includes.lowerCase = confirm("Do you want to include lowercase characters?")
-  includes.upperCase = confirm("Do you want to include uppercase characters?")
+  alert("You need to include at least one character type!");
+  includes.special = confirm("Do you want to include special characters?");
+  includes.numeric = confirm("Do you want to include numeric characters?");
+  includes.lowerCase = confirm("Do you want to include lowercase characters?");
+  includes.upperCase = confirm("Do you want to include uppercase characters?");
 }
+
+// If statements to determine whether a character type is being used, and if true push the type into the selectedCharacters array using getPasswordOptions function
 if (includes.special===true){
   getPasswordOptions(specialCharacters)
 }
